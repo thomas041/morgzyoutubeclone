960916620f2ff632a622eea64a687f367d92b326
Thomas Morgan <tom@morgz.co.uk>
2020-05-25 13:41:51 +0100

    POSTRESQL added

--- youtube_python/settings.py
+++ youtube_python/settings.py
@@ -62,10 +62,21 @@ WSGI_APPLICATION = 'youtube_python.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'vids',
+        'USER': 'tom',
+        'PASSWORD': ' ',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
