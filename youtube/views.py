b83286b0ef6dd5745d551b97bf84aef488e428e4
Thomas Morgan <tom@morgz.co.uk>
2020-05-25 11:07:19 +0100

    more shit

--- youtube/views.py
+++ youtube/views.py
@@ -9,11 +9,13 @@ from django.core.files.storage import FileSystemStorage
 import os
 from wsgiref.util import FileWrapper
 
+
 class VideoFileView(View):
-    
+
     def get(self, request, file_name):
         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        file = FileWrapper(open(BASE_DIR+'/'+file_name, 'rb'))
+
+        file = FileWrapper(open(BASE_DIR + '/' + file_name, 'rb'))
         response = HttpResponse(file, content_type='video/mp4')
         response['Content-Disposition'] = 'attachment; filename={}'.format(file_name)
         return response
@@ -21,31 +23,34 @@ class VideoFileView(View):
 
 class HomeView(View):
     template_name = 'index.html'
+
     def get(self, request):
         most_recent_videos = Video.objects.order_by('-datetime')[:8]
-        return render(request, self.template_name, {'menu_active_item': 'home', 'most_recent_videos': most_recent_videos})
+        return render(request, self.template_name,
+                      {'menu_active_item': 'home', 'most_recent_videos': most_recent_videos})
+
 
 class LogoutView(View):
     def get(self, request):
         logout(request)
         return HttpResponseRedirect('/')
 
+
 class VideoView(View):
     template_name = 'video.html'
 
     def get(self, request, id):
-        #fetch video from DB by ID
+        # fetch video from DB by ID
         video_by_id = Video.objects.get(id=id)
         BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-        video_by_id.path = 'http://localhost:8000/get_video/'+video_by_id.path
-        context = {'video':video_by_id}
-        
+        video_by_id.path = 'http://localhost:8000/get_video/' + video_by_id.path
+        context = {'video': video_by_id}
+
         if request.user.is_authenticated:
             print('user signed in')
             comment_form = CommentForm()
             context['form'] = comment_form
 
-        
         comments = Comment.objects.filter(video__id=id).order_by('-datetime')[:5]
         print(comments)
         context['comments'] = comments
@@ -54,10 +59,10 @@ class VideoView(View):
 
 class LoginView(View):
     template_name = 'login.html'
-    
+
     def get(self, request):
         if request.user.is_authenticated:
-            #logout(request)
+            # logout(request)
             return HttpResponseRedirect('/')
 
         form = LoginForm()
@@ -79,6 +84,7 @@ class LoginView(View):
                 return HttpResponseRedirect('login')
         return HttpResponse('This is Login view. POST Request.')
 
+
 class CommentView(View):
     template_name = 'comment.html'
 
@@ -90,15 +96,16 @@ class CommentView(View):
             text = form.cleaned_data['text']
             video_id = request.POST['video']
             video = Video.objects.get(id=video_id)
-            
+
             new_comment = Comment(text=text, user=request.user, video=video)
             new_comment.save()
             return HttpResponseRedirect('/video/{}'.format(str(video_id)))
         return HttpResponse('This is Register view. POST Request.')
 
+
 class RegisterView(View):
     template_name = 'register.html'
-    
+
     def get(self, request):
         if request.user.is_authenticated:
             print('already logged in. Redirecting.')
@@ -122,20 +129,21 @@ class RegisterView(View):
             return HttpResponseRedirect('/login')
         return HttpResponse('This is Register view. POST Request.')
 
+
 class NewVideo(View):
     template_name = 'new_video.html'
 
     def get(self, request):
         if request.user.is_authenticated == False:
-            #return HttpResponse('You have to be logged in, in order to upload a video.')
+            # return HttpResponse('You have to be logged in, in order to upload a video.')
             return HttpResponseRedirect('/register')
-        
+
         form = NewVideoForm()
-        return render(request, self.template_name, {'form':form})
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request):
         # pass filled out HTML-Form from View to NewVideoForm()
-        form = NewVideoForm(request.POST, request.FILES)       
+        form = NewVideoForm(request.POST, request.FILES)
 
         if form.is_valid():
             # create a new Video Entry
@@ -143,10 +151,12 @@ class NewVideo(View):
             description = form.cleaned_data['description']
             file = form.cleaned_data['file']
 
-            random_char = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
-            path = random_char+file.name
+            random_char = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+            path = random_char + file.name
 
-            fs = FileSystemStorage(location = os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+            fs = FileSystemStorage(location=os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+            # fs = FileSystemStorage(location='media')
+
             filename = fs.save(path, file)
             file_url = fs.url(filename)
 
@@ -154,14 +164,13 @@ class NewVideo(View):
             print(filename)
             print(file_url)
 
-            new_video = Video(title=title, 
-                            description=description,
-                            user=request.user,
-                            path=path)
+            new_video = Video(title=title,
+                              description=description,
+                              user=request.user,
+                              path=path)
             new_video.save()
-            
+
             # redirect to detail view template of a Video
             return HttpResponseRedirect('/video/{}'.format(new_video.id))
         else:
             return HttpResponse('Your form is not valid. Go back and try again.')
-
--- youtube_python/settings.py
+++ youtube_python/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '067dowgvbbhxh670i8#gox(_pq37*yj(wjt)9yg@+dz-)oggpr'
+SECRET_KEY = '067dowgvbbhxh670i8#gox(_pq37*yj(wjt)9yg@+dz-)oggprthisi448ddkltjkvkdlgkldag'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'youtube_python.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['youtube'],
+        'DIRS': ['templates/youtube/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,10 +68,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -108,3 +104,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'static/',
+]
+
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
