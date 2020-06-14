ec2ffb607bc616db718b075b57ceb5a130fabec1
Te@ <me@morgz.co.uk>
2020-06-14 17:33:52 +0000

    testing on UBUNTU machine

--- youtube_python/settings.py
+++ youtube_python/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = '067dowgvbbhxh670i8#gox(_pq37*yj(wjt)9yg@+dz-)oggprthisi448ddkltjkv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost', '10.4.2.105', 'study-manjaro.morgz.local', ]
+ALLOWED_HOSTS = ['*','0.0.0.0', '127.0.0.1', 'localhost', '10.4.2.105', 'study-manjaro.morgz.local', ]
 
 # Application definition
 
